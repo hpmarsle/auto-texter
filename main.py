272263d60c1cac753266446a9a6b4b25ff5d8b4e
Maria Le <hp.mars.le@gmail.com>
2021-01-05 16:17:26 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+HAPPY_BIRTHDAY_MESSAGES = [
+    "Happy birthday!",
+    "Happy birthday! I hope your day is filled with joy and love today!",
+    "Happy birthday! I hope you are doing well!",
+    "It's your birthday! It's your birthday! We gonna party like it's your birthday!",
+    "Happy Birthday! Have your cake and eat it too!"
+]
