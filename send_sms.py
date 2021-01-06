74c7eda8b60702226d17f11d3e72646df3165818
Maria Le <hp.mars.le@gmail.com>
2021-01-05 21:50:23 -0600

    Define send_message function with a message list parameter to send a random text from the list

--- send_sms.py
+++ send_sms.py
@@ -10,3 +10,9 @@ auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 to_number = os.environ.get("to_number")
 twilio_number = os.environ.get("twilio_number")
+
+def send_message(message_options):
+    random_message = message_options[random.randint(0, len(message_options))]
+    client.messages.create(to=to_number,
+                           from_=twilio_number,
+                           body=random_message)
